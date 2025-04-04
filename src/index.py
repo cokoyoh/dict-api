c6ed8c015355ff35f105cb8a8e112f75c04a98ea
cokoyoh <charlesokoyoh@gmail.com>
2025-04-04 06:22:22 +0300

    reformat project using black

--- src/index.py
+++ src/index.py
@@ -1,17 +1,21 @@
-print(f'Data to test...')
+print(f"Data to test...")
+
 
 def trial():
-  x = 3
-  print(f'x = {x}')
-  
+    x = 3
+    print(f"x = {x}")
+
+
 trial()
 
+
 class Trial:
-  def __init__(self):
-    print(f'Initializiing class...')
-  
-  def build(self):
-    print(f'Class fully initiated...')
-  
+    def __init__(self):
+        print(f"Initializiing class...")
+
+    def build(self):
+        print(f"Class fully initiated...")
+
+
 obj = Trial()
 obj.build()
