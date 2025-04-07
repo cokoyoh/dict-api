e2f4d48be2be775b9149f832aa985e1c7708f877
cokoyoh <charlesokoyoh@gmail.com>
2025-04-07 12:14:21 +0300

    First principles to understanding path and query params

--- src/main.py
+++ src/main.py
@@ -7,9 +7,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "hello world!"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+  return {"user_id": user_id}
