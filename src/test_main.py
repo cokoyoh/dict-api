ff823c2555ed1d8a9e39571ea5dabd5ed0018e3a
cokoyoh <charlesokoyoh@gmail.com>
2025-04-10 10:30:35 +0300

    Format files

--- src/test_main.py
+++ src/test_main.py
@@ -3,7 +3,8 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
-  response = client.get('/')
-  assert response.status_code == 200
-  assert response.json() == {"message": "hello world!"}
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "hello world!"}
